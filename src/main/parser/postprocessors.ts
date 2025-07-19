b28a0a2e843afb6a69dfe5b2e9214913606be5e6
Madeline Kahn <mk-acct@outlook.com>
2025-07-19 01:41:47 -0400

    Add postprocessors

---
+++ src/main/parser/postprocessors.ts
@@ -0,0 +1,21 @@
+// Included parser postprocessor functions you may find convenient
+
+import * as ss from "superstruct";
+
+import {ParserPostprocessorFunction, struct_ParserPostprocessorFunction} from "./decoder.js";
+
+/**
+ * Parser postprocessor that applies the given postprocessor function if the parsed value has the given type name.
+ * @param type the type identifier to postprocess
+ * @param fn the postprocessor function to apply if the type matches
+ * @returns the postprocessor function
+ */
+export function postprocessType(type: string, fn: ParserPostprocessorFunction): ParserPostprocessorFunction {
+	if (!ss.is(type, ss.string())) {throw new TypeError(`type must be a string`);}
+	if (!ss.is(fn, struct_ParserPostprocessorFunction)) {throw new TypeError(`fn must be a ParserPostprocessorFunction`);}
+	return ({key, type: actualType, value}) => {
+		if (actualType === type) {
+			return fn({key, type: actualType, value});
+		}
+	};
+}
